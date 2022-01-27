1a42d65fbe7623ae7351d596c8b3d077d9fefb7c
nyaco0125 <catpacross@gmail.com>
2022-01-27 22:21:14 +0900

    헤로쿠는 진짜 해로워요

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.use((error, req, res, next) => {
     res.sendStatus(500);
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
