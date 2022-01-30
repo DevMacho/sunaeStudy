2aaa9dbb454acd959a1a7379f2436ac2029d6508
nyaco0125 <catpacross@gmail.com>
2022-01-30 14:05:15 +0900

    cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors({credentials: true,}));
+app.use(cors());
 
 app.use('/posts', postRoute);
 app.use('/auth', authRoute);
