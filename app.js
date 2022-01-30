27554ef4357950dc932c4a2034a28f6cddd224da
nyaco0125 <catpacross@gmail.com>
2022-01-30 14:17:04 +0900

    해로운 헤로쿠에 이어서 콜라비로 등짝 스매싱 마려운 콜스네요

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000/',
+    origin: '*',
     optionsSuccessStatus: 200
 }));
 
