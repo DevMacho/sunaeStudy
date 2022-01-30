f4e2e13ee81c75be769e8ddfc8864b0e3e226fbc
nyaco0125 <catpacross@gmail.com>
2022-01-30 21:38:32 +0900

    프로필 라우터 수정

--- router/profile.js
+++ router/profile.js
@@ -6,10 +6,10 @@ import * as profileController from '../controller/profile.js'
 
 const router = express.Router();
 
-router.get('/profile', isAuth, profileController);
-router.put('/editId', isAuth, profileController);
-router.put('/editStudentId', isAuth, profileController);
-router.put('/editPassword', isAuth, profileController);
-router.put('/editProfileImg', isAuth, profileController);
+router.get('/profile', isAuth, profileController.viewProfile);
+router.put('/editId', isAuth, profileController.editId);
+router.put('/editStudentId', isAuth, profileController.editStudentId);
+router.put('/editPassword', isAuth, profileController.editPassword);
+router.put('/editProfileImg', isAuth, profileController.editProfileImg);
 
 export default router;
